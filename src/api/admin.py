492bf8e5a55f10fcf22b5b0f6a164328e1081462
matiseni51 <matiseni51@hotmail.com>
2022-03-01 15:04:50 -0300

    model refined and data for admin added

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,3 +1,47 @@
 from django.contrib import admin
+from api.models import *
 
 # Register your models here.
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_filters = ('type')
+    list_display = ('name', 'address', 'email', 'cellphone', 'type')
+    search_field = ('name', 'email', 'cellphone')
+
+
+@admin.register(Container)
+class ContainerAdmin(admin.ModelAdmin):
+    list_filters = ('type', 'liters')
+    list_display = ('type', 'liters')
+
+
+@admin.register(Flavour)
+class FlavourAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'price_per_lt')
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_filters = ('state')
+    list_display = ('code', 'arrived_date', 'price', 'state')
+    search_field = ('code', 'container__type', 'container__liters', 'flavour__name')
+
+
+@admin.register(Quota)
+class QuotaAdmin(admin.ModelAdmin):
+    list_display = ('current_quota', 'total_quota', 'value', 'date')
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_filters = ('method')
+    list_display = ('transaction', 'amount', 'quotas', 'method')
+    search_field = ('transaccton', 'amount')
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_filters = ('state')
+    list_display = ('date', 'price', 'delivery_cost', 'total_amount', 'customer', 'state')
+    search_field = ('customer__name')
--- src/api/models.py
+++ src/api/models.py
@@ -2,37 +2,46 @@ from django.db import models
 
 # Create your models here.
 
-class Customers(models.Model):
+class Customer(models.Model):
     _type = [
-        ('PA','Particular'),
-        ('CO','Comerce'),
+        ('Particular','Particular'),
+        ('Comerce','Comerce'),
     ]
     name = models.CharField(max_length=30)
     address = models.CharField(max_length=120)
     email = models.EmailField()
     cellphone = models.CharField(max_length=12)
     type = models.CharField(
-        max_length=2, choices=_type, default='CO')
+        max_length=10, choices=_type, default='CO')
+
+    def __str__(self):
+        return '%s (%s)' % (self.name, self.type)
 
 
-class Containers(models.Model):
+class Container(models.Model):
     _type = [
-        ('K', 'Keg'),
-        ('B', 'Bottle')
+        ('Keg', 'Keg'),
+        ('Growler', 'Growler'),
+        ('Bottle', 'Bottle')
     ]
     _available_lts = {
         'Keg': [20, 30, 50],
+        'Growler': [2],
         'Bottle': [0.5, 1, 2]
     }
 
     type = models.CharField(
-        max_length=1, choices=_type, default='K')
-    liters = models.IntegerField()
+        max_length=7, choices=_type, default='Keg')
+    liters = models.DecimalField(max_digits=4, decimal_places=2)
 
     def select_lts(self):
         return self._available_lts[self.type]
 
-class Flavours(models.Model):
+    def __str__(self):
+        return '%s - %s lt' % (self.type, self.liters)
+
+
+class Flavour(models.Model):
     # _flavour = [
     #     ('YAC','Yacare'),
     #     ('YAR','Yarara'),
@@ -46,58 +55,74 @@ class Flavours(models.Model):
     description = models.TextField()
     price_per_lt = models.DecimalField(max_digits=10, decimal_places=2)
 
+    def __str__(self):
+        return '%s' % (self.name)
 
-class Products(models.Model):
+
+class Product(models.Model):
     _state = [
-        ('ST','In Stock'),
-        ('TR','In Transit'),
-        ('RE','Return & Empty'),
+        ('In Stock','In Stock'),
+        ('In Transit','In Transit'),
+        ('Empty','Empty'),
     ]
-    code = models.CharField(max_length=5)
-    containers = models.ForeignKey(Containers, on_delete=models.CASCADE)
-    flavour = models.ForeignKey(Flavours, on_delete=models.CASCADE)
+    code = models.CharField(
+        max_length=5, help_text='Code graven in Kegs')
+    container = models.ForeignKey(Container, on_delete=models.CASCADE)
+    flavour = models.ForeignKey(Flavour, on_delete=models.CASCADE)
     arrived_date = models.DateTimeField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     state = models.CharField(
-        max_length=2, choices=_state, default='ST')
+        max_length=10, choices=_state, default='ST')
+
+    def __str__(self):
+        return '%s - %s' % (self.container, self.flavour)
 
 
-class Quotas(models.Model):
+class Quota(models.Model):
     current_quota = models.IntegerField()
     total_quota = models.IntegerField()
     value = models.DecimalField(max_digits=10, decimal_places=2)
     date = models.DateField()
 
+    def __str__(self):
+        return '%s/%s' % (self.current_quota, self.total_quota)
 
-class Payments(models.Model):
+
+class Payment(models.Model):
     _method = [
-        ('DC', 'Debit Card'),
-        ('CC', 'Credit Card'),
-        ('CA', 'Cash'),
-        ('TR', 'Bank Transfer'),
-        ('DW', 'Digital Wallet'),
-        ('CR', 'Cryptocurrency'),
+        ('Debit Card', 'Debit Card'),
+        ('Credit Card', 'Credit Card'),
+        ('Cash', 'Cash'),
+        ('Bank Transfer', 'Bank Transfer'),
+        ('Digital Wallet', 'Digital Wallet'),
+        ('Cryptocurrency', 'Cryptocurrency'),
     ]
     transaction = models.IntegerField()
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     method = models.CharField(
-        max_length=2, choices=_method, default='CA')
-    quotas = models.ForeignKey(Quotas, on_delete=models.CASCADE)
+        max_length=14, choices=_method, default='CA')
+    quotas = models.ForeignKey(Quota, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.transaction
 
 
-class Orders(models.Model):
+class Order(models.Model):
     _state = [
-        ('PE', 'Pending'),
-        ('QU', 'In Quotas'),
-        ('PA', 'Paid'),
+        ('Pending', 'Pending'),
+        ('In Quotas', 'In Quotas'),
+        ('Paid', 'Paid'),
     ]
     date = models.DateField()
-    products = models.ForeignKey(Products, on_delete=models.CASCADE)
+    products = models.ForeignKey(Product, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     delivery_cost = models.DecimalField(max_digits=10, decimal_places=2)
     total_amount = models.DecimalField(max_digits=10, decimal_places=2)
-    customer = models.ForeignKey(Customers, on_delete=models.CASCADE)
-    payment = models.OneToOneField(Payments, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    payment = models.OneToOneField(Payment, on_delete=models.CASCADE)
     state = models.CharField(
-        max_length=2, choices=_state, default='GE')
+        max_length=9, choices=_state, default='GE')
     comment = models.TextField()
+
+    def __str__(self):
+        return '%s - %s' % (self.date, self.customer)
