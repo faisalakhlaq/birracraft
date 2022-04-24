fb9e07cfd0177f84ab95152b3ecc47523c33d7b5
matiseni51 <matiseni51@hotmail.com>
2022-04-24 13:47:34 +0200

    Ref: allow permission for /docs and new token path

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 
 from rest_framework.documentation import include_docs_urls
+from rest_framework.permissions import AllowAny
 from rest_framework.routers import SimpleRouter
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -18,8 +19,8 @@ router.register(r'quota', views.QuotaViewSet, basename='quota')
 router.register(r'order', views.OrderViewSet, basename='order')
 
 urlpatterns = [
-    path('docs/', include_docs_urls(title='API Birracraft')),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('docs/', include_docs_urls(title='API Birracraft', permission_classes=[AllowAny,])),
+    path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', include((router.urls, 'Birracraft'), namespace='Birracraft')),
 ]
