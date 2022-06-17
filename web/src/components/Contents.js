b3f0f8c675894ba5aa1e2c36cf95ddceb65d7aa0
matiseni51 <matiseni51@hotmail.com>
2022-06-17 19:07:32 +0200

    Ref: checking js identation in contents file

--- web/src/components/Contents.js
+++ web/src/components/Contents.js
@@ -24,30 +24,30 @@ import Profile from './Profile.js';
 
 
 const Contents = () => (
-    <Routes>
-				<Route exact path="/" element={<Home />} />
-        <Route exact path="/SignIn" element={<SignIn />} />
-        <Route exact path="/SignUp" element={<SignUp />} />
-        <Route exact path="/RegistrationSuccess" element={<RegistrationSuccess />} />
-        <Route exact path="/RegistrationFail" element={<RegistrationFail />} />
-        <Route exact path="/ActivationSuccess" element={<ActivationSuccess />} />
-        <Route exact path="/ActivationFail" element={<ActivationFail />} />
-        <Route exact path="/ResetPass" element={<ResetPass />} />
-        <Route exact path="/ResetPassForm" element={<ResetPassForm />} />
-        <Route exact path="/ResetPassSent" element={<ResetPassSent />} />
-        <Route exact path="/ResetPassInvalidLink" element={<ResetPassInvalidLink />} />
-        <Route exact path="/ResetPassSuccess" element={<ResetPassSuccess />} />
-        <Route exact path="/ResetPassFail" element={<ResetPassFail />} />
-        <Route exact path="/" element={<PrivateContent />}>
-					<Route exact path="/Orders" element={<Orders />} />
-					<Route exact path="/Payments" element={<Payments />} />
-					<Route exact path="/Products" element={<Products />} />
-          <Route exact path="/ContainersFlavours" element={<ContainersFlavours />} />
-					<Route exact path="/Customers" element={<Customers />} />
-          <Route exact path="/Report" element={<Report />} />
-					<Route exact path="/Profile" element={<Profile />} />
-				</Route>
-    </Routes>
+  <Routes>
+    <Route exact path="/" element={<Home />} />
+    <Route exact path="/SignIn" element={<SignIn />} />
+    <Route exact path="/SignUp" element={<SignUp />} />
+    <Route exact path="/RegistrationSuccess" element={<RegistrationSuccess />} />
+    <Route exact path="/RegistrationFail" element={<RegistrationFail />} />
+    <Route exact path="/ActivationSuccess" element={<ActivationSuccess />} />
+    <Route exact path="/ActivationFail" element={<ActivationFail />} />
+    <Route exact path="/ResetPass" element={<ResetPass />} />
+    <Route exact path="/ResetPassForm" element={<ResetPassForm />} />
+    <Route exact path="/ResetPassSent" element={<ResetPassSent />} />
+    <Route exact path="/ResetPassInvalidLink" element={<ResetPassInvalidLink />} />
+    <Route exact path="/ResetPassSuccess" element={<ResetPassSuccess />} />
+    <Route exact path="/ResetPassFail" element={<ResetPassFail />} />
+    <Route exact path="/" element={<PrivateContent />}>
+      <Route exact path="/Orders" element={<Orders />} />
+      <Route exact path="/Payments" element={<Payments />} />
+      <Route exact path="/Products" element={<Products />} />
+      <Route exact path="/ContainersFlavours" element={<ContainersFlavours />} />
+      <Route exact path="/Customers" element={<Customers />} />
+      <Route exact path="/Report" element={<Report />} />
+      <Route exact path="/Profile" element={<Profile />} />
+    </Route>
+  </Routes>
 );
 
 export default Contents;
